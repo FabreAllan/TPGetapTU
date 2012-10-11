56bd2b2b90267087cc6bbe3a408d567f7ccd6cb9
afabre <afabre@btssio.vinci-melun.org>
2012-10-11 15:59:57 +0200

    PHASE 3 question 3

---
+++ TPGetapTU/Test/org/ldv/sio/getap/DemandeTest.java
@@ -0,0 +1,29 @@
+package org.ldv.sio.getap;
+
+import static org.junit.Assert.*;
+
+import java.sql.Date;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DemandeTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		DemandeValidationConsoTempsAccPers dvctap = new DemandeValidationConsoTempsAccPers(
+				1L, 
+				"2012-2013", 
+				Date.valueOf("07-10-2012"), 
+				20, 
+				new User(1L,"Capuozzo", "Olivier",new Classe(1,"SIO22"),"prof-principal","okpu","toto","okpu@gmail.fr"),
+				new AccPersonalise(),
+				new User(2L,"Ben Ragdel", "Nizar",new Classe(1,"SIO22"),"eleve","nini","toto","nizar@gmail.fr"), 
+				0);
+	}
+
+}
---
+++ TPGetapTU/src/org/ldv/sio/getap/Exemple.java
@@ -0,0 +1,30 @@
+package org.ldv.sio.getap;
+
+import java.sql.Date;
+
+class Exemple {
+	
+	private DemandeValidationConsoTempsAccPers dvctap;
+	
+	static public void main(String[] args) {
+		Classe classe  = new Classe(1,"SIO22");
+		User prof =  new User(1L,"Capuozzo", "Olivier",classe,"prof-principal","okpu","toto","okpu@gmail.fr");
+		User eleve =  new User(2L,"Ben Ragdel", "Nizar",classe,"eleve","nini","toto","nizar@gmail.fr");
+		AccPersonalise accPerso = new AccPersonalise(1,"Salon du libre",1,1L);
+		Date date = Date.valueOf("07-10-2012");
+		DemandeValidationConsoTempsAccPers dvctap = new DemandeValidationConsoTempsAccPers(1L, "2012-2013", date, 20, prof,accPerso,eleve, 0);
+		System.out.println(dvctap);
+		dvctap.setEtat(4);
+		System.out.println(dvctap);
+		dvctap.setEtat(32);
+		System.out.println(dvctap);
+		dvctap.setEtat(4);
+		System.out.println(dvctap);
+		dvctap.setEtat(2048);
+		System.out.println(dvctap);
+		dvctap.setEtat(2);
+		System.out.println(dvctap);
+		dvctap.setEtat(32);
+		System.out.println(dvctap);
+	}
+}
