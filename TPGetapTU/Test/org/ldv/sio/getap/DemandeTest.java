8e2f69bf739147ed4fa29bfb1abb29179cbffae8
usb <usb@btssio.vinci-melun.org>
2012-10-12 10:32:29 +0200

    prob

--- TPGetapTU/Test/org/ldv/sio/getap/DemandeTest.java
+++ TPGetapTU/Test/org/ldv/sio/getap/DemandeTest.java
@@ -15,15 +15,12 @@ public class DemandeTest {
 
 	@Test
 	public void test() {
-		DemandeValidationConsoTempsAccPers dvctap = new DemandeValidationConsoTempsAccPers(
-				1L, 
-				"2012-2013", 
+		DemandeValidationConsoTempsAccPers dvctap = new DemandeValidationConsoTempsAccPers(1L, "2012-2013", 
 				Date.valueOf("07-10-2012"), 
 				20, 
 				new User(1L,"Capuozzo", "Olivier",new Classe(1,"SIO22"),"prof-principal","okpu","toto","okpu@gmail.fr"),
 				new AccPersonalise(),
-				new User(2L,"Ben Ragdel", "Nizar",new Classe(1,"SIO22"),"eleve","nini","toto","nizar@gmail.fr"), 
-				0);
+				new User(2L,"FABRE", "ALLAN",new Classe(1,"SIO22"),"eleve","nini","toto","nizar@gmail.fr"),0);
 		
 		assertTrue("Etat initial", dvctap.isEtatInitial());
 	}
