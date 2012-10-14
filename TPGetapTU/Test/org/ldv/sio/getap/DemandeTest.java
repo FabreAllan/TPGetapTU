983a16baa06af021cd35755c982ffc7f5b7bb4b8
Allan Fabre <fabre.allan@yahoo.fr>
2012-10-14 16:39:36 +0200

    Erreur Test

--- TPGetapTU/Test/org/ldv/sio/getap/DemandeTest.java
+++ TPGetapTU/Test/org/ldv/sio/getap/DemandeTest.java
@@ -4,22 +4,27 @@ import static org.junit.Assert.*;
 
 import java.sql.Date;
 
+import junit.framework.TestCase;
+
 import org.junit.Before;
 import org.junit.Test;
 
-public class DemandeTest {
-	DemandeValidationConsoTempsAccPers dvctap = new DemandeValidationConsoTempsAccPers(
-			1L, 
-			"2012-2013", 
-			Date.valueOf("07-10-2012"), 
-			20, 
-			new User(1L,"Capuozzo", "livier",new Classe(1,"SIO22"),"prof-principal","okpu","toto","okpu@gmail.fr"),
-			new AccPersonalise(),
-			new User(2L,"Ben Ragdel", "Nizar",new Classe(1,"SIO22"),"eleve","nini","toto","nizar@gmail.fr"), 
-			0);
-
+public class DemandeTest extends TestCase{
+	
+	private DemandeValidationConsoTempsAccPers dvctap;
+	
 	@Before
-	public void setUpBefore() throws Exception {
+	public void setUp() throws Exception {
+		Classe classe = new Classe(1, "SIO22");
+		Discipline discipline = new Discipline(1, "SLAM4");
+		User eleve =  new User(2L,"Ben Ragdel", "Nizar",classe,"eleve","nini","toto","nizar@gmail.fr");
+		User prof =  new User(1L,"Capuozzo", "Olivier",classe,"prof-principal","okpu","toto","okpu@gmail.fr");
+		
+		AccPersonalise accPers = new AccPersonalise(1, "Salon du libre", 0, 1l);
+		Date date = Date.valueOf("2012-10-07");
+
+		dvctap = new DemandeValidationConsoTempsAccPers(1l, "2012-2013", date,
+				240, prof, accPers, eleve, 0);
 	}
 
 	@Test
@@ -30,6 +35,42 @@ public class DemandeTest {
 		catch(DVCTAPException e){
 		}
 
+	}
+	
+	public void testEtatDifferents() {
+		try{
+			dvctap.modifieeParEleve();
+			dvctap.modifieeParEleve();
+			dvctap.modifieeDateParLeProfesseur();
+			dvctap.modifieeDureeParLeProfesseur();
+			dvctap.accepteeParEleve();
+			assertFalse("Demande accepté par l'élève!", dvctap.accepteeParEleve());
+		}
+		catch(DVCTAPException e){
+		}
+
+	}
+	
+	public void testModEleveValidProf() {
+		try{
+			dvctap.setEtat(0);
+			dvctap.modifieeParEleve();
+			dvctap.valideeParLeProfesseur();
+			assertFalse("Demande valider par le professeur!", dvctap.valideeParLeProfesseur());
+		}
+		catch(DVCTAPException e){
+		}
+
+	}
+	
+	public void testCreRejet(){
+		try{
+			dvctap.setEtat(0);
+			dvctap.rejeteParEleve();
+			assertFalse("Demande rejeter par l'élève", dvctap.rejeteParEleve());
+		}
+		catch(DVCTAPException e){
+		}
 	}
 
 }
--- TPGetapTU/src/org/ldv/sio/getap/Exemple.java
+++ TPGetapTU/src/org/ldv/sio/getap/Exemple.java
@@ -8,11 +8,14 @@ class Exemple {
 
 	static public void main(String[] args) {
 		Classe classe  = new Classe(1,"SIO22");
-		User prof =  new User(1L,"Capuozzo", "Olivier",classe,"prof-principal","okpu","toto","okpu@gmail.fr");
+		Discipline discipline = new Discipline(1, "SLAM4");
+		User prof =  new User(1L,"Capuozzo", "Olivier",classe,"prof-principal",discipline);
 		User eleve =  new User(2L,"Ben Ragdel", "Nizar",classe,"eleve","nini","toto","nizar@gmail.fr");
 		AccPersonalise accPerso = new AccPersonalise(1,"Salon du libre",1,1L);
 		Date date = Date.valueOf("07-10-2012");
+		
 		DemandeValidationConsoTempsAccPers dvctap = new DemandeValidationConsoTempsAccPers(1L, "2012-2013", date, 20, prof,accPerso,eleve, 0);
+		
 		System.out.println(dvctap);
 		dvctap.setEtat(4);
 		System.out.println(dvctap);
