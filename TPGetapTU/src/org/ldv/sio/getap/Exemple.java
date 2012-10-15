ba63f779e5727b89b5173d87b1a8267ff0518d3d
Allan Fabre <fabre.allan@yahoo.fr>
2012-10-15 20:00:11 +0200

    Derniere modification

--- TPGetapTU/src/org/ldv/sio/getap/Exemple.java
+++ TPGetapTU/src/org/ldv/sio/getap/Exemple.java
@@ -14,7 +14,7 @@ class Exemple {
 		Classe classe  = new Classe(1,"SIO22");
 		Discipline discipline = new Discipline(1, "SLAM4");
 		User prof =  new User(1L,"Capuozzo", "Olivier",classe,"prof-principal",discipline);
-		User eleve =  new User(2L,"Ben Ragdel", "Nizar",classe,"eleve","nini","toto","nizar@gmail.fr");
+		User eleve =  new User(2L,"Ben Ragdel", "Nizar",classe,"eleve");
 		AccPersonalise accPerso = new AccPersonalise(1,"Salon du libre",1,1L);
 		Date date = Date.valueOf("07-10-2012");
 		
